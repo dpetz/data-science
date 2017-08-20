e4ae7eca652d954f8da37323bbf06e520987ddfe
„dpetzgit config --global user.name „dpetz <Dirk.Petzoldt@gmail.com>
2017-08-20 22:50:52 +0200

    Started "Repeat"

--- ds-scala/src/io/Parser.scala
+++ ds-scala/src/io/Parser.scala
@@ -1,33 +1,40 @@
 package io
 
-import io.Parser.{Consume, Rex}
+import io.Parser.{Cons, Rex}
 
+import scala.annotation.tailrec
 import scala.collection.mutable.ArrayBuffer
 
 
 object Parser {
 
   /** Result of applying Parser to Stream */
-  sealed class Parsed[A] {
-    /** Map result */
+  sealed trait Parsed[A] {
+    /** Map result if Match. */
     def map[B](f:A=>B):Parsed[B]
+    /** Map complete match if Match. */
+    def apply[B](f:(Match[A]=>Parsed[B])):Parsed[B]
+
+    def follow:Reader
+
+    def atFail(f:Fail[A]=>Parsed[A]):Parsed[A]
   }
 
   /** @param follow : Reader after parsed content has been consumed */
-  case class Match[A](result: A, follow: Reader) extends Parsed[A] {
-
-    /** Appends results of p to results of this. Returns NoMatch if p is NoMatch */
-    def <<[A](p:Parsed[A]):Parsed[Seq[A]]=
-      p match {
-        case m2:Match[A] => Match(List(result, m2.result), m2.follow)
-        case _ => NoMatch[Seq[A]]
-      }
-
+  case class Match[A](result: A,follow: Reader) extends Parsed[A] {
     def map[B](f:A=>B)=Match(f(result),follow)
+    def apply[B](f:(Match[A]=>Parsed[B]))=f(this)
+    def atFail(f:Fail[A]=>Parsed[A]) = this
   }
 
-  case class NoMatch[A]() extends Parsed[A] {
-    def map[B](f:A=>B)=NoMatch[B]
+  case class Fail[A](parser:Parser[_], follow: Reader) extends Parsed[A] {
+    def map[B](f:A=>B)=Fail[B](this)
+    def apply[B](f:Match[A]=>Parsed[B])=Fail[B](this)
+    override def toString=s"$parser failed parsing: $follow"
+    def atFail(f:Fail[A]=>Parsed[A])=f(this)
+  }
+  object Fail {
+    def apply[B](f:Fail[_]):Fail[B]=Fail[B](f.parser, f.follow)
   }
 
   /* Can be applied to a Stream to produce a Match[A] */
@@ -55,63 +62,75 @@ object Parser {
   }
 
   /** Parse predefined string */
-  class Consume(val s: String) extends Parser[String] {
-    def apply(r: Reader) = {
-      Chars(s.length)(r) match {
-        case m: Match[String] => if (m.result == s) m else NoMatch()
-        case n:NoMatch[String] => n //n:NoMatch[String] => n
+  class Cons(val s: String) extends Parser[String] {
+    def apply(r: Reader) =
+      Chars(s.length)(r)  { m =>
+        if (m.result == s) m else Fail(this,r)
       }
-    }
+    override def toString=s"Cons($s)"
   }
 
   /** Any character of given set */
-  case class Any(c: Set[Char]) extends Parser[Char] {
+  case class OneOf(c: Set[Char]) extends Parser[Char] {
     def apply(r: Reader) =
       if (c(r.char)) Match(r.char, r.next)
-      else NoMatch()
+      else Fail(this,r)
   }
 
-
-
-  implicit class Infix(s:String) extends Consume(s)
+  implicit class Str2Cons(s:String) extends Cons(s)
 
   implicit class Parser2SeqParser[A](p:Parser[A]) extends Parser[Seq[A]] {
     def apply(r:Reader)=p(r).map(List(_))
   }
 
   /** p1 followed by p2 **/
-  case class Then[A](p1: Parser[A], p2: Parser[A]) extends Parser[Seq[A]] {
+  case class Then[A](p1: Parser[Seq[A]], p2: Parser[Seq[A]]) extends Parser[Seq[A]] {
     def apply(r: Reader) =
-      p1(r) match {
-        case m1: Match[A] => m1 << p2(m1.follow)
-        case _ => NoMatch[Seq[A]]
+      p1(r) { m1 =>
+        p2(m1.follow) { m2 =>
+          Match[Seq[A]](m1.result ++ m2.result, m2.follow)
+        }
       }
   }
 
   /** Find prefix Regex match within next n characters of stream  */
-  case class Rex(reStr:String,n:Int) extends Parser[String] {
-    val re = reStr.r
+  case class Rex(n:Int)(reStr:String) extends Parser[String] {
+
     def apply(r:Reader)=
-      re.findPrefixMatchOf(r.next(n)) match {
+      reStr.r.findPrefixMatchOf(r.next(n)) match {
         case Some(m) => Chars(m.end)(r)
-        case None => NoMatch[String]
+        case None => Fail(this,r)
       }
+
+    override def toString=s"Rex($n)($reStr)"
   }
+
+  case class Repeat[A](p:Parser[A], min:Int=0, max:Int=Int.MaxValue) extends Parser[Seq[A]] {
+
+    def apply(r:Reader)={
+
+      @tailrec
+      def repeatNext(p:Parser[A], min:Int,max:Int, ms:Match[Seq[A]]):Parsed[Seq[A]]=
+        p(r) map { List(_,ms.result) } { m =>
+          if (max > 1) repeatNext(p, min - 1, max - 1, m)
+          else m
+        } atFail { f =>
+          if (min<1) ms else Fail[B](this)
+      }
+    }
+      repeatNext(p,min,max,Nil)
+    }
+
+  }
+
+
 }
 
 object Test {
 
   def and {
-    import io.Parser.Infix
-    val r = Reader("Hallo Welt7")
-    val p = "Hallo" ~ " Welt" ~ Rex("\d")
+    import io.Parser.Str2Cons
+    val r = Reader("Hallo Welt 2017")
+    val p = "Hallo" ~ " Welt " ~ Rex(2)("""\d+""")
     println(p(r).toString)
   }
-  /*
-  val reader = Reader("Hallo Welt")
-  val m = Parser.Chars(6)(reader)
-  m.toString
-  val m2 = Parser.Consume("Welt")(m.follow)
-  m2.toString
-  */
-}
--- ds-scala/src/io/Reader.scala
+++ ds-scala/src/io/Reader.scala
@@ -11,29 +11,35 @@ trait Reader {
   def next:Reader
   /** Check if next character */
   def hasNext:Boolean
+  /** Position since start (zero based) */
+  def pos:Int
 
   /** Peek into next n characters */
   def next(n:Int):String = {
     /** Accumulator parameter to allow recursive call optimization.
       * See https://stackoverflow.com/questions/6005392/isnt-that-code-in-tail-recursive-style */
     @tailrec
-    def nextAsList(l:List[Char], r:Reader, n:Int):List[Char] =
-      if (n==1) r.char :: l
-      else nextAsList(r.char :: l, r.next, n-1)
-    nextAsList(Nil, this,n).toString
+    def nextAsList(l:List[Char], r:Reader, n:Int):Seq[Char] = {
+      if (!r.hasNext) return ">DNE<".toCharArray ++ l
+      if (n==0) return l
+      return nextAsList(r.char :: l, r.next, n-1)
+    }
+    nextAsList(Nil, this,n).reverse.mkString
   }
+
+  override def toString=s"Reader(${next(5)}...)"
 }
 
 object Reader {
 
   /** Implement via lazy Source. Not exposed to preserve immutability */
-  private class SourceReader(src:Source) extends Reader {
-    lazy val char:Char=src.next()
-    lazy val hasNext:Boolean=src.hasNext
-    lazy val next:Reader= {
+  private class SourceReader(src:Source,val pos:Int) extends Reader {
+    lazy val char=src.next
+    lazy val hasNext=src.hasNext
+    lazy val next= {
       char // trigger src.next()
-      new SourceReader(src)
+      new SourceReader(src,pos+1)
     }
   }
-  def apply(s:String):Reader=new SourceReader(Source.fromString(s))
+  def apply(s:String):Reader=new SourceReader(Source.fromString(s),0)
 }
